b89e952f55463ce00e3690886a2486a4fb072e80
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-06-01 00:01:01 +0300

    get dashboard elements

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+// Select HTML Elements
+// Countdown dashboard
+const decadesEl = document.getElementById("decades");
+const yearsEl = document.getElementById("years");
+const monthsEl = document.getElementById("months");
+const weeksEl = document.getElementById("weeks");
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+// Time is Out message
+const messEl = document.getElementById("mess");
